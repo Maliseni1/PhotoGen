f04bbdadc42fe0f54085df3c5fedd166187b9946
Maliseni1 <maliseni1205@gmail.com>
2025-10-27 17:57:07 +0200

    More changes on the contact details

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -19,22 +19,22 @@ const Footer = () => {
 
         <div className='flex gap-2.5'>
             {/* Facebook */}
-            <a href="https://www.facebook.com/YOUR_PAGE" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.facebook.com/Mariseny.moves/" target="_blank" rel="noopener noreferrer">
               <img src={assets.facebook_icon} alt='Facebook' width={35} className='cursor-pointer invert dark:invert-0' />
             </a>
 
             {/* Twitter */}
-            <a href="https://www.twitter.com/YOUR_PROFILE" target="_blank" rel="noopener noreferrer">
+            <a href="https://x.com/MAlisenichavula/" target="_blank" rel="noopener noreferrer">
               <img src={assets.twitter_icon} alt='Twitter' width={35} className='cursor-pointer invert dark:invert-0' />
             </a>
 
             {/* Instagram */}
-            <a href="https://www.instagram.com/YOUR_PROFILE" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/malisenichavula/" target="_blank" rel="noopener noreferrer">
               <img src={assets.instagram_icon} alt='Instagram' width={35} className='cursor-pointer invert dark:invert-0' />
             </a>
 
             {/* LinkedIn (New) */}
-            <a href="https://www.linkedin.com/in/YOUR_PROFILE" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/maliseni-chavula-7100b323b" target="_blank" rel="noopener noreferrer">
               <img src={assets.linkedin_icon} alt='LinkedIn' width={35} className='cursor-pointer invert dark:invert-0' />
          </a>
         </div>
